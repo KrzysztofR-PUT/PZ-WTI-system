7dd19afab72f6831c3504192ccbc79fb57d8501a
KrzysztofR-PUT <krzysztof.rozga@student.put.poznan.pl>
2016-03-21 22:50:05 +0100

    Wersja początkowa (szkielet)

---
+++ ScholarshipWebApplication/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ScholarshipWebApplication
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ ScholarshipWebApplication/Controllers/PresidentSchController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ScholarshipWebApplication.Controllers
+{
+    public class PresidentSchController : Controller
+    {
+        // GET: PresidentSch
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
