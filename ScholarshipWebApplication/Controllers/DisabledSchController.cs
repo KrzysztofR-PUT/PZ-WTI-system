8ab9713cad350ecf341c75f95db2a3bb58b6fb40
KrzysztofR-PUT <krzysztof.rozga@student.put.poznan.pl>
2016-03-22 11:16:04 +0100

    Zmieniony szkielet (polaczenie PZ i WTI)

---
+++ ScholarshipWebApplication/Controllers/DisabledSchController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ScholarshipWebApplication.Controllers
+{
+    public class DisabledSchController : Controller
+    {
+        // GET: DisabledSch
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ScholarshipWebApplication/Controllers/ScholarshipController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ScholarshipWebApplication.Controllers
+{
+    public class ScholarshipController : Controller
+    {
+        // GET: Scholarship
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
