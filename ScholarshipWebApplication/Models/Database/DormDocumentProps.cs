bd5466a3d2837bce36da849367f67c2e232251e6
Radosław Kapłon <radziuk96@gmail.com>
2016-04-09 21:46:26 +0200

    Dodanie atrybutów do klas(wyświetlanie nazw, typ dancyh)

--- ScholarshipWebApplication/Models/Database/DormDocumentProps.cs
+++ ScholarshipWebApplication/Models/Database/DormDocumentProps.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace ScholarshipWebApplication.Models.Database
 {
@@ -6,12 +7,20 @@ namespace ScholarshipWebApplication.Models.Database
     {
         [Key]
         public int DocID { get; set; }
+
+        [DisplayName("Dystans od domu")]
         public int DistanceFromHome { get; set; }
+        [DisplayName("Trudności dojazdu")]
         public string TransportDifficulties { get; set; }
+        [DisplayName("Liczba członków rodziny")]
         public int FamilyCount { get; set; }
+        [DisplayName("Przychód na osobę")]
         public float IncomePerPerson { get; set; }
+        [DisplayName("Czy student jest niepełnosprawny?")]
         public bool IsDisabled { get; set; }
+        [DisplayName("Czy student jest z rodziny niepełnej?")]
         public bool isFullFamily { get; set; }
+        [DisplayName("Rok studiów")]
         public string CurrentAcademicYear { get; set; }
         public virtual Student student { get; set; }
     }
--- ScholarshipWebApplication/Models/Database/Student.cs
+++ ScholarshipWebApplication/Models/Database/Student.cs
@@ -1,22 +1,32 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace ScholarshipWebApplication.Models.Database
 {
     public class Student
     {
+       
+        [Key]
         public int StudentID { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Imię jest wymagane")]
+        [DisplayName("Imię")]
         public string FirstName { get; set; }
 
-        [Required]
+        [Required(ErrorMessage ="Nazwisko jest wymagane")]
+        [DisplayName("Nazwisko")]
         public string LastName { get; set; }
 
         [Required]
+        [DisplayName("NrAlbumu")]
+        [StringLength(6,ErrorMessage ="Nr albumu ma 6 Cyfr !")]
+       
         public string RegistryNumber { get; set; } //nr albumu
-
+        [DataType(DataType.PhoneNumber,ErrorMessage ="Popraw Nr Telefonu")]
+        [DisplayName("Tel.")]
         public string PhoneNumber { get; set; }
-
+        [DataType(DataType.EmailAddress, ErrorMessage ="Niepoprawny email. Poprawny: jozek@gmail.com")]
+        [DisplayName("e-mail")]
         public string Email { get; set; }
 
         public virtual Address address { get; set; }
