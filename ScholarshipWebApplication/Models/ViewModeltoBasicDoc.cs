56f91c14d0a7b98a675eedf9b69843de8775557e
Radosław Kapłon <radziuk96@gmail.com>
2016-04-10 15:09:46 +0200

    Utworzenie podstawowego dokumentu

---
+++ ScholarshipWebApplication/Models/ViewModeltoBasicDoc.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ScholarshipWebApplication.Models.Database;
+
+namespace ScholarshipWebApplication.Models
+{
+    public class ViewModeltoBasicDoc
+    {
+        public Address Adres { get; set; }
+        public Student Student { get; set; }
+
+    }
+}
