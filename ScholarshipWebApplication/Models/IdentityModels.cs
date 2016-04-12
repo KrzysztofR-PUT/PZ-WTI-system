597983ae0385752da15a30b1a3988b30456dcf7a
KrzysztofR-PUT <krzysztof.rozga@student.put.poznan.pl>
2016-04-12 20:40:23 +0200

    Dodany widok dla stypendium niepełnosprawnych, zmiana z tym laczeniem z lokalnej na azure

--- ScholarshipWebApplication/Models/IdentityModels.cs
+++ ScholarshipWebApplication/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace ScholarshipWebApplication.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("StudentContext", throwIfV1Schema: false)
         {
         }
 
