e29790913116a7aa879583a5ae0b452328464246
Adrian <adrian_kaczmarek94@wp.pl>
2016-05-01 15:14:58 +0200

    Poprawki w socjalce

--- ScholarshipWebApplication/Controllers/ScholarshipController.cs
+++ ScholarshipWebApplication/Controllers/ScholarshipController.cs
@@ -5,6 +5,8 @@ using ScholarshipWebApplication.Models.Database;
 using System.Web.Mvc;
 using System.Linq;
 using System.Collections.Generic;
+using System.Data.Entity.Validation;
+using System.Diagnostics;
 
 namespace ScholarshipWebApplication.Controllers
 {
